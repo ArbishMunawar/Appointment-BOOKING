ff7c21c8b42a87947553c3296712da89e7418d62
ArbishMunawar <167678183+ArbishMunawar@users.noreply.github.com>
2025-08-17 22:29:54 -0700

    commit

--- Backend/models/doctor.model.js
+++ Backend/models/doctor.model.js
@@ -13,7 +13,7 @@ const doctorSchema = new mongoose.Schema(
     available: { type: Boolean, default: true },
     fees: { type: Number, default: true },
     address: { type: Object, required: true },
-    date: { type: Number, required: true },
+    // date: { type: Number, required: true },
    slots_booked: { type: Object, default: {} },
   },
   { minimize: false, timestamps:true} 
--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,8 @@ import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import setupRoutes from './routes/routes.js';
-import path from "path";
+
+
 //app config
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,7 +20,6 @@ app.use(express.json());
 
 //api endpoints
 setupRoutes(app);
-app.use("/public", express.static(path.join(__dirname, "public")));
 app.get('/', (req, res) => res.status(200).send('Hello ARBISH!'));
 
 
